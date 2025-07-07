90e565b2cc422654d1f705c489744b50c3fa6782
saia632 <Saia.chan100@gmail.com>
2025-07-07 09:56:38 +0200

    نقاطي.js

---
+++ plugins/commands/economy/نقاطي.js
@@ -0,0 +1,20 @@
+const config = {
+  name: "نقاطي",
+  aliases: ["نقاط", "رصيدي"],
+  description: "عرض عدد النقاط التي تملكها",
+  usage: "",
+  cooldown: 3,
+  category: "فعاليات",
+  credits: "XaviaTeam"
+};
+
+async function onCall({ message }) {
+  const { Users } = global.controllers;
+  const points = await Users.getData(message.senderID, "eventPoints") || 0;
+  return message.reply(`🎯 نقاطك الحالية: ${points} نقطة`);
+}
+
+export default {
+  config,
+  onCall
+};
